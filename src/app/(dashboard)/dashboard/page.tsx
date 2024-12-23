abaf5e334102eb3a086ec00aa019ced33debafcb
Priyangsu Banik <priyangsubanik2003@gmail.com>
2024-12-23 21:03:47 +0530

    Update page.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import Header from "@/components/Header";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <>{/* <Header /> */}</>;
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
 };
 
 export default page;
